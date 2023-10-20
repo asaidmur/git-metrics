a36180c3dcc492eb881e452865f9326edeba08cf
asaidmur <asaidmur@mbp.local>
2023-10-19 18:10:26 -0700

    dev added controlles for open, closed, and merged PRs

--- src/main/kotlin/com/gitmetrics/app/service/GitHubService.kt
+++ src/main/kotlin/com/gitmetrics/app/service/GitHubService.kt
@@ -6,15 +6,37 @@ import org.springframework.stereotype.Service
 import java.nio.file.Files
 import java.nio.file.Paths
 
+private const val ALL_PR_QUERY = "src/main/kotlin/com/gitmetrics/app/queries/AllPullRequests.json"
+private const val CLOSED_PR_QUERY = "src/main/kotlin/com/gitmetrics/app/queries/ClosedPullRequests.json"
+private const val OPEN_PR_QUERY = "src/main/kotlin/com/gitmetrics/app/queries/OpenPullRequests.json"
+private const val MERGED_PR_QUERY = "src/main/kotlin/com/gitmetrics/app/queries/MergedPullRequests.json"
+
 @Service
 class GitHubService(private val gitHubClient: GitHubFeignClient) {
 
-    fun fetchAllPRData(owner: String, repo: String): Map<String, Any> {
-        val queryFilePath = Paths.get("src/main/kotlin/com/gitmetrics/app/queries/PullRequests.json")
-        var query = String(Files.readAllBytes(queryFilePath))
-        query = String.format(query, owner, repo)
-        val token = Token.getToken()
-        val response = gitHubClient.executeQuery(token, query)
+    fun fetchAllPullRequests(owner: String, repo: String): Map<String, Any> {
+        val query = generateQuery(owner, repo, ALL_PR_QUERY)
+        val response = gitHubClient.executeQuery(Token.getToken(), query)
         return response
+    }
+    fun fetchClosedPullRequests(owner: String, repo: String): Map<String, Any> {
+        val query = generateQuery(owner, repo, CLOSED_PR_QUERY)
+        val response = gitHubClient.executeQuery(Token.getToken(), query)
+        return response
+    }
+    fun fetchOpenPullRequests(owner: String, repo: String): Map<String, Any> {
+        val query = generateQuery(owner, repo, OPEN_PR_QUERY)
+        val response = gitHubClient.executeQuery(Token.getToken(), query)
+        return response
+    }
+    fun fetchMergedPullRequests(owner: String, repo: String): Map<String, Any> {
+        val query = generateQuery(owner, repo, MERGED_PR_QUERY)
+        val response = gitHubClient.executeQuery(Token.getToken(), query)
+        return response
+    }
+    private fun generateQuery(owner: String, repo: String, queryTemplate: String): String {
+        val queryFilePath = Paths.get(queryTemplate)
+        var query = String(Files.readAllBytes(queryFilePath))
+        return String.format(query, owner, repo)
     }
 }
